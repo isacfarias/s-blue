4c6d19ba622f588fba6d93aa750da960280d4f2e
isacfarias <isac.farias.21@gmail.com>
2023-12-29 15:50:22 -0400

    realizando conversões dos tipos de dados

---
+++ demo-one/src/main/kotlin/first/step/numericos/ConversoresNumericos.kt
@@ -0,0 +1,15 @@
+package first.step.numericos
+
+fun main() {
+    val dado = 10
+    val pentadado = dado.toDouble()
+    println(pentadado)
+
+    val jaca = 10.5
+    val vaca = jaca.toInt()
+    println(vaca)
+
+    val bola = 2000
+    val areias = bola.toByte()
+    println(areias)
+}
--- demo-one/src/main/kotlin/first/step/numericos/Numericos.kt
+++ demo-one/src/main/kotlin/first/step/numericos/Numericos.kt
@@ -2,16 +2,23 @@ package first.step.numericos
 
 fun main() {
     /*
-    Byte:  1 byte - 8 bits
-    Short: 2 byte - 16 bites
-    Int: 4 bytes  - 32 bits
-    long: 8 byte  - 64 bites
+    Byte:   1 byte - 8 bits
+    Short:  2 byte - 16 bits
+    Int:    4 bytes  - 32 bits
+    long:   8 byte  - 64 bits
+    Float:  4 bytes - 32 bits
+    Double: 8 bytes - 64 bits
      */
 
     val cachorro = 10
     val gato: Int = 20
+    val passaro = 10L //define que será long, o L precisa ser maiusculo
 
-    val passaro = 10L // define que será long, o L precisa ser maiusculo
+    val galinha = 14.5 // é definido como double
+    var granja: Double = 20.8
+    var jacaré = 7.5F//definido como float
+    var pirarucu: Float  = 9.73F //complexo, mesmo sendo um tipo declarado como float, tem que botar o F
+
 
 
 }
