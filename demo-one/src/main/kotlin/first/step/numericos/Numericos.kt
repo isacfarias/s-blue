f6107fa3e3e78315ba9689c45daf8a0d339d056d
isacfarias <isac.farias.21@gmail.com>
2023-12-29 10:51:41 -0400

    tipo de dados numericos inteiros

---
+++ demo-one/src/main/kotlin/first/step/numericos/Numericos.kt
@@ -0,0 +1,17 @@
+package first.step.numericos
+
+fun main() {
+    /*
+    Byte:  1 byte - 8 bits
+    Short: 2 byte - 16 bites
+    Int: 4 bytes  - 32 bits
+    long: 8 byte  - 64 bites
+     */
+
+    val cachorro = 10
+    val gato: Int = 20
+
+    val passaro = 10L // define que será long, o L precisa ser maiusculo
+
+
+}
