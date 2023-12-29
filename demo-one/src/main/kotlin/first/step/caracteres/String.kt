88d6da17dd6d0a9d02cf0e868c8c8ca553c658a8
isacfarias <isac.farias.21@gmail.com>
2023-12-29 16:36:33 -0400

    String e String templates

---
+++ demo-one/src/main/kotlin/first/step/caracteres/String.kt
@@ -0,0 +1,10 @@
+package first.step.caracteres
+
+fun main() {
+    var s1 = "abc"
+    var s2 = "def"
+//    val s1s2 = "Soma: "+ (s1 + s2)
+    val s1s2 = "Soma: '$s1$s2'. O Tamanho é : ${(s1 + s2).length} "
+
+    println(s1s2)
+}
