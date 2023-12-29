b645b5df14b0899e1c39522375c582c84f1b5376
isacfarias <isac.farias.21@gmail.com>
2023-12-29 10:41:06 -0400

    tipos de variaveis e suas caracteristicas

---
+++ demo-one/src/main/kotlin/first/step/variaveis/Variaveis.kt
@@ -0,0 +1,19 @@
+package first.step.variaveis
+
+fun main() {
+
+    //var counter: Int
+    //counter = 10
+
+    var counter: Int = 10
+    counter = 5
+
+    println("valor é [$counter]")
+
+    //val é imutavel
+    val counterVal: Int = 10
+
+    println("valor é [$counterVal]")
+
+
+}
