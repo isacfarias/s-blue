a5143d952a032a434a305a5668671e46d12aa5b4
isacfarias <isac.farias.21@gmail.com>
2023-12-29 16:40:13 -0400

    usando caracteres de escape

---
+++ demo-one/src/main/kotlin/first/step/caracteres/TravaLingua.kt
@@ -0,0 +1,15 @@
+package first.step.caracteres
+
+fun main() {
+
+    /**
+     * O rato roeu
+     * a roupa do
+     * rei de "Roma"
+     */
+
+    val str = "O rato roeu\na roupa do\nrei de \"Roma\""
+
+    println(str)
+
+}
