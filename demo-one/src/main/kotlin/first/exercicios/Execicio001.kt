17e3a2ad8df7e5606befd40f55a89e1360f5c22c
isacfarias <isac.farias.21@gmail.com>
2024-04-20 17:32:55 -0400

    execicio de fixação

---
+++ demo-one/src/main/kotlin/first/exercicios/Execicio001.kt
@@ -0,0 +1,23 @@
+package first.exercicios
+
+import java.util.*
+
+/**
+ * EXERCÍCIO
+ * ---------
+ *
+ * Solicite 3 informaçãoes do usuario: nome, idade e peso, Depois imprima uma frase que contenha
+ * essas informações (use string templates para manter a frase).
+ **/
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    print("Informe o nome > ")
+    val nome = scanner.next()
+    print("Informe o idade > ")
+    val idade = scanner.nextInt()
+    print("Informe o peso > ")
+    val peso = scanner.nextBigDecimal()
+
+    println("Olá sou(a) ${nome} tenho ${idade} anos e peso ${peso} Kg.")
+}
--- demo-one/src/main/kotlin/first/step/inputs/KeyboardInput.kt
+++ demo-one/src/main/kotlin/first/step/inputs/KeyboardInput.kt
@@ -1,7 +1,19 @@
 package first.step.inputs
 
+import java.util.Scanner
+
 fun main() {
+    /*
     print("Gigite seu nome:")
     var nome = readln()
     println("seu nome invertido é ${nome.reversed()}")
+    */
+
+    val scanner = Scanner(System.`in`)
+    print("> ")
+    val n1 = scanner.nextInt()
+    print("> ")
+    val n2 = scanner.nextInt()
+
+    println("valores ${n1 + n2}")
 }
