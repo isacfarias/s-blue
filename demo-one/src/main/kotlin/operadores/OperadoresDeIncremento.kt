5b547d5d05bfb6511a65c75bdc7304da3323a301
isacfarias <isac.farias.21@gmail.com>
2024-04-20 19:30:58 -0400

    incremento e decremento

---
+++ demo-one/src/main/kotlin/operadores/OperadoresDeIncremento.kt
@@ -0,0 +1,27 @@
+package operadores
+
+fun main() {
+
+    println("PRE incremento")
+    var x = 8
+    println(x)
+    println(x++)
+    println(x)
+
+    println("POS incremento")
+    var y = 10
+    println(y)
+    println(++y)
+
+
+    println("PRE decremento")
+    var b = 15
+    println(b)
+    println(b--)
+    println(b)
+
+    println("POS decremento")
+    var c = 20
+    println(c)
+    println(--c)
+}
