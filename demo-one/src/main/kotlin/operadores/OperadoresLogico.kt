7e761cf14757d774c0f5d8ee10e89e571a7da833
isacfarias <isac.farias.21@gmail.com>
2024-04-20 19:11:59 -0400

    operadores OR e NOT

--- demo-one/src/main/kotlin/operadores/OperadoresLogico.kt
+++ demo-one/src/main/kotlin/operadores/OperadoresLogico.kt
@@ -1,17 +1,42 @@
 package operadores
 /**
  * >> TABELA DA VERDADE<<
+ *
  * && -> AND -> E
- * true && true
- * true && false
- * false && true
- * false && false
+ *        T AND T = true
+ *        T AND F = false
+ *        F AND T = false
+ *        F AND F = false
+ *
+ * || -> OR -> OU
+ *        T OR T = true
+ *        T OR F = false
+ *        F OR T = false
+ *        F OR F = false
+ *
+ * !<?> -> NOT -> NÃO
+ *        NOT T = false
+ *        NOT F = true
+
  **/
 fun main() {
 
-    println("T AND T ${true && true}")
-    println("T AND F ${true && false}")
-    println("F AND T ${false && true}")
-    println("F AND F ${false && false}")
-
+    //AND
+    println("&& -> AND -> E")
+    println("T AND T = ${true && true}")
+    println("T AND F = ${true && false}")
+    println("F AND T = ${false && true}")
+    println("F AND F = ${false && false}")
+    println()
+    //OR
+    println("|| -> OR -> OU")
+    println("T OR T = ${true && true}")
+    println("T OR F = ${true && false}")
+    println("F OR T = ${false && true}")
+    println("F OR F = ${false && false}")
+    println()
+    //NOT
+    println("!<?> -> NOT -> NÃO")
+    println("NOT T = ${!true}")
+    println("NOT F = ${!false}")
 }
