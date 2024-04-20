f4bfa297f06f9cc96d44c23620832f8eab3afd68
isacfarias <isac.farias.21@gmail.com>
2024-04-20 19:24:40 -0400

    operadores de atibuição

---
+++ demo-one/src/main/kotlin/operadores/OperadoresDeAtribuicao.kt
@@ -0,0 +1,16 @@
+package operadores
+
+fun main() {
+
+    var x = 5
+
+    x += 2 //x = x + 2
+    println(x)
+    x *= 2 //x = x * 2
+    println(x)
+    x -= 2 //x = x - 2
+    println(x)
+    x /= 3 //x = x / 2
+    println(x)
+
+}
