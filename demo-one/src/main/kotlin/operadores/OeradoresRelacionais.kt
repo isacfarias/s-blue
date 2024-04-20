802b6a7f857fa9a62b6ca42e01f8f7b77d37b534
isacfarias <isac.farias.21@gmail.com>
2024-04-20 18:16:02 -0400

    operadores relacionais

---
+++ demo-one/src/main/kotlin/operadores/OeradoresRelacionais.kt
@@ -0,0 +1,20 @@
+package operadores
+
+//Operadores relacionais sempre vai retorar um boleano
+fun main() {
+
+    //igual igual
+    println("10 == 10 ís ${10 == 10}")
+    //diferente
+    println("10 != 7 ís ${10 != 7}")
+    //Maior que
+    println("5 > 2 ís ${5 > 2}")
+    //Menor que
+    println("5 < 2 ís ${5 < 2}")
+    //Maior ou igual
+    println("11 >= 10 ís ${11 >= 10}")
+    //Menor ou igual
+    println("5 <= 10 ís ${5 <= 10}")
+
+
+}
