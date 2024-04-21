4248267e79d849f35f1da5875c10a2d39454f421
isacfarias <isac.farias.21@gmail.com>
2024-04-20 22:44:07 -0400

    wip

--- demo-one/src/main/kotlin/com/farias/estrutura/de/selecao/CondicionalWhen.kt
+++ demo-one/src/main/kotlin/com/farias/estrutura/de/selecao/CondicionalWhen.kt
@@ -44,4 +44,27 @@ fun main() {
         value < 0 -> println("NEGATIVO")
         else -> println("ZERO")
     }
+
+
+    print("Informe um valor > ")
+    value = readln().toInt()
+
+   var result =  when {
+        value > 0 -> "POSITIVO"
+        value < 0 -> "NEGATIVO"
+        else -> "ZERO"
+    }
+
+    println("Resultado $result")
+
+    print("Informe um dos valores (A, B) > ")
+    var str = readln()[0]
+
+    result =  when(str) {
+        'A', 'a' -> "AAAAAAAAA"
+        'B', 'b' -> "BBBBBBBBB"
+        else -> "Não sei qume é"
+    }
+
+    println("Resultado $result")
 }
